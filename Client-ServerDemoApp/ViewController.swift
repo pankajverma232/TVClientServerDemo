349bd53825b07c10b3c2a99def04354432958d20
Pankaj Verma <pankaj.verma@tavant.com>
2016-01-28 14:56:07 +0530

    Initial Commit

---
+++ Client-ServerDemoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Client-ServerDemoApp
+//
+//  Created by Pankaj Verma on 28/01/16.
+//  Copyright © 2016 Pankaj Verma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
